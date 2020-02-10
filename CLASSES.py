5d6faa8a98fd1d6ed13b6345e3e7bce4ace3ccc6
madbosskay <60551824+madbosskay@users.noreply.github.com>
2020-02-10 20:12:28 +0100

    Create CLASSES.py

---
+++ CLASSES.py
@@ -0,0 +1,24 @@
+class PronetMall():
+    def __init__(self,industry, name):
+        self.industry = industry
+        self.name = name
+        
+early_code = PronetMall(industry = 'ICT', name = 'Joseph')
+tribe_c.industry
+PronetMall
+
+#class name: Cars
+#Object: Honda, Mercedez, Ford
+#Attribute: Honda --> 4WD, Mercedez --> Auto Cruise, Ford --> Auto Navigator
+
+#class: circles
+#class attributes: pi = 3.142
+#attribute = radius
+#class methods: area, circumference
+#object: circle, circle2
+
+circle1 = Circles(radius = 24)
+circle2 = Circles(radius = 23)
+
+
+
